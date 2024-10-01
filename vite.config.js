0a08f0ff61ac7227ded5457846a594dba3780a90
Owais Mehmood <20-arid-815@student.uaar.edu.pk>
2024-10-01 17:43:53 +0500

    updated verceel configs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,18 @@ import react from "@vitejs/plugin-react";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString(); // Split by package names
+          }
+        },
+      },
+    },
+  },
   plugins: [react(), reactRefresh()],
 });
