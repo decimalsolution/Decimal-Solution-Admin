e710aadf3bb5f555a7174cd3f1c118986b67b0d6
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2025-01-10 15:14:45 +0500

    Update backend URL in constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,12 +1,9 @@
-export const backendUrl = "https://backend.decimalsolution.com";
-
-
+// export const backendUrl = "https://backend.decimalsolution.com";
 
 // export const backendUrl = "http://localhost:5000";
 
+export const backendUrl = "https://decimal-backend-7f6b055d5bae.herokuapp.com";
 
 // export const backendUrl = "https://4glkps52-5000.inc1.devtunnels.ms"
-
-
 
 // export const backendUrl = "https://ds-backend-zeta.vercel.app";
