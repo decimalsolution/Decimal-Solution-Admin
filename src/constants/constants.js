0216dff8f3b6bc961e3a880260d9cd4efb90e56f
laraib-decimal <laraibabbasi@decimalsolution.com>
2026-07-31 19:23:20 +0500

    Feat: Created cloudinary file and integrated it in the Dropzone component. Also removed the firebase file

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,8 +1,10 @@
 // export const backendUrl = "https://backend.decimalsolution.com";
 
-// export const backendUrl = "http://localhost:5000";
+export const backendUrl = "http://localhost:5000";
+export const cloudinaryCloudName = "xfoqfqjg"
+export const cloudinaryCloudPreset = "decimal-web"
 
-export const backendUrl = "https://decimal-backend-7f6b055d5bae.herokuapp.com";
+// export const backendUrl = "https://decimal-backend-7f6b055d5bae.herokuapp.com";
 
 // export const backendUrl = "https://4glkps52-5000.inc1.devtunnels.ms"
 
